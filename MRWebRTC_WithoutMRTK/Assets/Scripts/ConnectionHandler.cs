8e186938deefd3c064e40f1e10a1071f81e2a9b4
lukas <willmann.Lukas@web.de>
2022-11-07 16:37:08 +0100

    Integrated connectionhandler to include more than one connection

--- MRWebRTC_WithoutMRTK/Assets/Scripts/ConnectionHandler.cs
+++ MRWebRTC_WithoutMRTK/Assets/Scripts/ConnectionHandler.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microsoft.MixedReality.WebRTC.Unity;
 
 public class ConnectionHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    
+    public List<GameObject> Connections;
+
+    public GameObject ConnectionPrefab;
+
+    public GameObject Model;
+
+    public GameObject ConnectionIPInput;
+
+    public GameObject ConnectionThumbPrefab;
+
     void Start()
     {
         
@@ -14,5 +26,18 @@ public class ConnectionHandler : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void AddConnection()
+    {
+        GameObject newConnection = Instantiate(ConnectionPrefab);
+        Connections.Add(newConnection);
+        DataChannelReceiver newConnectionDataChannelReceiver = newConnection.transform.GetChild(0).gameObject.GetComponent<DataChannelReceiver>();
+        newConnectionDataChannelReceiver.model = Model;
+
+        NodeDssSignaler newConNodeDSSSignaler = newConnection.transform.GetChild(1).gameObject.GetComponent<NodeDssSignaler>();
+        newConNodeDSSSignaler.LocalPeerId = "sender" + (Connections.Count - 1);
+        newConNodeDSSSignaler.RemotePeerId = "receiver" + (Connections.Count - 1);
+        newConNodeDSSSignaler.HttpServerAddress = "http://" + ConnectionIPInput.GetComponent<TMPro.TMP_InputField>().text + ":3000/";
     }
 }
